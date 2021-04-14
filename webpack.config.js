97d495bd286068de0efd37d315ba9f400b002554
Fabi0pi <spairasee@gmail.com>
2021-04-14 11:02:51 +0200

    adding files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './dist/js/index.js',
+  entry: './js/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
